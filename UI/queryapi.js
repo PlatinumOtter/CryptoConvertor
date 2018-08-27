0ace0d6a7a49467602ebf9de1596fe23b8264a61
Daniel Jackson <peaceinchaos13@gmail.com>
2018-08-26 23:13:32 -0500

    final updates

--- UI/queryapi.js
+++ UI/queryapi.js
@@ -1,27 +1,27 @@
 function getamount() {
-    var coin1 = document.getElementById("coin1");
+    var coin1 = document.getElementById("coin1").value;
     var coin1rate = 1;
-    var coin2 = document.getElementById("coin2");
+    var coin2 = document.getElementById("coin2").value;
     var coin2rate = 1;
-    var exchange = document.getElementById("exchange");
-    var starting = document.getElementById("amount");
+    var exchange = document.getElementById("exchange").value;
+    var starting = document.getElementById("amount").value;
 
     $.ajax({
-        url: "localhost:5000/api/v1.0/" + exchange + "?coin=" + coin1,
+        url: "http://localhost:5000/api/v1.0/" + exchange + "?coin=" + coin1,
         success: function(data) {
-            var obj = JSON.parse(data)
-            coin1rate = obj.rate
+            var obj = JSON.parse(data);
+            coin1rate = obj.rate;
         }
-    })
+    });
     $.ajax({
-        url: "localhost:5000/api/v1.0/" + exchange + "?coin=" + coin2,
+        url: "http://localhost:5000/api/v1.0/" + exchange + "?coin=" + coin2,
         success: function(data) {
-            var obj = JSON.parse(data)
-            coin2rate = obj.rate
+            var obj = JSON.parse(data);
+            coin2rate = obj.rate;
         }
-    })
+    });
 
-    var result = starting * coin1rate / coin2rate
+    var result = starting * coin1rate / coin2rate;
 
-    document.getElementById("result").innerHTML = result
+    document.getElementById("result").innerHTML = result;
 }
